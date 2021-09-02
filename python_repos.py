315bf5088f8ab4bda44d86b7f9be2698c3ffa36c
iPortillo <ismaelpa91@gmail.com>
2021-09-02 18:41:44 +0200

    Edit python_repos.py, examine every repo's data.

--- python_repos.py
+++ python_repos.py
@@ -18,10 +18,9 @@ print(f"Repositories returned: {len(repo_dicts)}")
 repo_dict = repo_dicts[0]
 
 print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}\n")
